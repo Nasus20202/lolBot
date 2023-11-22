8dfa774f80ff53b40e7a194abc799a292613c7fe
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-11-22 01:28:19 +0100

    feat: update to new riot IDs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 from dotenv import load_dotenv
 import os
@@ -14,24 +15,43 @@ def main():
     intents.guilds = True
     intents.messages = True
 
+    # Riot API constants
+    server = "eun1"
+    region = "europe"
+    defaultTagline = "EUNE"
+
     bot = commands.Bot(command_prefix='!', intents=intents)
-    riot_client = riot_api.RiotAPI(os.getenv('RIOT_TOKEN'))
+    riot_client = riot_api.RiotAPI(os.getenv('RIOT_TOKEN'), server, region)
 
     @bot.event
     async def on_ready():
-        print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+        log(f'Logged in as {bot.user} (ID: {bot.user.id})')
         await bot.change_presence(status=discord.Status.online, activity=discord.Game("League of Legends"))
 
     @bot.command()
     async def level(ctx, *, arg):
-        summoner = await riot_client.get_summoner_by_name(arg)
+        log_command(ctx, arg)
+
+        gameName, tagLine = split_gametag(arg)
+        if(gameName is None or tagLine is None):
+            await ctx.send(invalid_riot_id())
+            return
+
+        puuid = await riot_client.get_riot_account_puuid(gameName, tagLine)
+        if(puuid is None):
+            await ctx.send(riot_account_not_found(gameName, tagLine))
+            return
+        
+        summoner = await riot_client.get_summoner_by_puuid(puuid)
         if(summoner["status_code"] == 200):
             await ctx.send(f'{summoner["name"]} is level {summoner["summonerLevel"]}')
         else:
-            await ctx.send(f'Summoner {arg} doesn\'t exsit!')
+            await ctx.send(summoner_not_found(gameName, tagLine))
 
     @bot.command(aliases=['last', 'm', 'l'])
     async def match(ctx, *, arg):
+        log_command(ctx, arg)
+
         id = arg.split(" ")[-1]
         isNumber = True
         for c in id:
@@ -44,15 +64,23 @@ def main():
             summoner_name += i
         if(not isNumber):
             summoner_name = arg
-        summoner = await riot_client.get_summoner_by_name(summoner_name)
-        if(summoner["status_code"] != 200):
-            await ctx.send(f"Summoner {arg} doesn't exist!")
+        
+        gameName, tagLine = split_gametag(summoner_name)
+        puuid = await riot_client.get_riot_account_puuid(gameName, tagLine)
+        if(puuid is None):
+            await ctx.send(riot_account_not_found(gameName, tagLine))
             return
+        
+        summoner = await riot_client.get_summoner_by_puuid(puuid)
+        if(summoner["status_code"] != 200):
+            await ctx.send(summoner_not_found(gameName, tagLine))
+            return
+
         if(int(id) > 100 or int(id) < 1):
             await ctx.send(f"You can only see your last 100 matches!")
             return
-        match_info = await riot_client.get_recent_match_info(summoner["name"], int(id)-1)
-        if(match_info == None):
+        match_info = await riot_client.get_recent_match_info(puuid, int(id)-1)
+        if(match_info is None):
             await ctx.send(f"Match not found!")
             return
         embed = await embed_generator.generate_match_embed(match_info, summoner["name"])
@@ -60,9 +88,20 @@ def main():
 
     @bot.command(aliases=['player', 'rank', 'user', 'p', 'r', 'u'])
     async def profile(ctx, *, arg):
-        data = await riot_client.get_profile_info(arg)
+        log_command(ctx, arg)
+
+        gameName, tagLine = split_gametag(arg)
+        if(gameName is None or tagLine is None):
+            await ctx.send(invalid_riot_id())
+            return
+        puuid = await riot_client.get_riot_account_puuid(gameName, tagLine)
+        if(puuid is None):
+            await ctx.send(riot_account_not_found(gameName, tagLine))
+            return
+
+        data = await riot_client.get_profile_info(puuid)
         if(data["status_code"] != 200):
-            await ctx.send(f"Summoner {arg} doesn't exist!")
+            await ctx.send(summoner_not_found(gameName, tagLine))
             return
         user = data["user"]
         embed = await embed_generator.generate_user_embed(user)
@@ -70,6 +109,8 @@ def main():
 
     @bot.command(aliases=['matches', 'h'])
     async def history(ctx, *, arg):
+        log_command(ctx, arg)
+
         count = arg.split(" ")[-1]
         isNumber = True
         for c in count:
@@ -82,14 +123,48 @@ def main():
             summoner_name = ""
             for i in arg.split(" ")[:-1]:
                 summoner_name += i
-        data = await riot_client.get_recent_matches_infos(summoner_name, int(count))
+
+        gameName, tagLine = split_gametag(summoner_name)
+        puuid = await riot_client.get_riot_account_puuid(gameName, tagLine)
+        if(puuid is None):
+            await ctx.send(riot_account_not_found(gameName, tagLine))
+            return
+        
+        data = await riot_client.get_recent_matches_infos(puuid, int(count))
         if (len(data[0]) <= 0):
-            await ctx.send(f"Summoner {arg} doesn't exist, you can only see your last 20 games")
+            await ctx.send(f"Summoner {gameName}#{tagLine} doesn't exist, you can only see your last 20 games")
             return
         embed = await embed_generator.generate_history_embed(data)
         await ctx.send(embed=embed)
 
+    def log_command(ctx, arg):
+        log(f'Command: [{ctx.command} {arg}] from [{ctx.author}] in [{ctx.guild}]')
+
+    def log(message, level="INFO"):
+        timestamp = datetime.datetime.now()
+        timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        print(f'[{timestamp_str}\t{level}] {message}')
+
+    def split_gametag(gametag):
+        hashtags = gametag.count("#")
+        if(gametag.count("#") > 1):
+            return None, None
+        if(hashtags == 0):
+            return gametag, defaultTagline
+        parts = gametag.split("#")
+        return parts[0], parts[1]
+    
+    def invalid_riot_id():
+        return f'Invalid Riot ID! (ex: username#tagline)'
+    
+    def riot_account_not_found(gameName, tagLine):
+        return f'Riot Account {gameName}#{tagLine} doesn\'t exsit!'
+    
+    def summoner_not_found(gameName, tagLine):
+        return f'Summoner {gameName}#{tagLine} doesn\'t exsit!'
+
     bot.run(os.environ.get('DISCORD_TOKEN'))
+    
 
 if __name__ == "__main__":
     main()
--- riot_api.py
+++ riot_api.py
@@ -5,13 +5,23 @@ class RiotAPI:
     queueTypes = {400: "Draft", 420: "Solo/Duo", 430: "Blind", 440: "Flex", 450: "ARAM", 700: "Clash"}
     queueWeight = {"UNRANKED": -1, "IRON": 0, "BRONZE": 1, "SILVER": 2, "GOLD": 3, "PLATINUM": 4, "EMERALD" : 5, "DIAMOND": 6, "MASTER": 7, "GRANDMASTER": 8, "CHALLENGER": 9}
 
-    def __init__(self, api_key, server="eun1", region="europe"):
+    def __init__(self, api_key, server, region):
         self.api_key = api_key
-        self.base_url = f'https://{server}.api.riotgames.com/lol/'
-        self.base_url_universal = f'https://{region}.api.riotgames.com/lol/'
+        self.base_url = f'https://{server}.api.riotgames.com/'
+        self.base_url_universal = f'https://{region}.api.riotgames.com/'
 
-    async def get_summoner_by_name(self, summoner_name):
-        url = f"{self.base_url}summoner/v4/summoners/by-name/{summoner_name}"
+    async def get_riot_account_puuid(self, gameName, tagLine):
+        url = f"{self.base_url_universal}riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}"
+        params = {'api_key': self.api_key}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as response:
+                data = await response.json()
+                if response.status == 200:
+                    return data["puuid"]
+                return None
+
+    async def get_summoner_by_puuid(self, puuid):
+        url = f"{self.base_url}lol/summoner/v4/summoners/by-puuid/{puuid}"
         params = {'api_key': self.api_key}
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params) as response:
@@ -23,7 +33,7 @@ class RiotAPI:
                 return data["status"]          
     
     async def get_matches_ids_by_puuid(self, puuid, count=20, start=0):
-        url = f"{self.base_url_universal}match/v5/matches/by-puuid/{puuid}/ids"
+        url = f"{self.base_url_universal}lol/match/v5/matches/by-puuid/{puuid}/ids"
         params = {'api_key': self.api_key, 'count': count, 'start': start}
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params) as response:
@@ -33,7 +43,7 @@ class RiotAPI:
                 return []
 
     async def get_raw_match_info_by_id(self, match_id):
-        url = f"{self.base_url_universal}match/v5/matches/{match_id}"
+        url = f"{self.base_url_universal}lol/match/v5/matches/{match_id}"
         params = {'api_key': self.api_key}
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params) as response:
@@ -44,8 +54,8 @@ class RiotAPI:
                     return data
                 return data["status"]
 
-    async def get_recent_matches_ids(self, username, count=20):
-        summoner_data = await self.get_summoner_by_name(username)
+    async def get_recent_matches_ids(self, puuid, count=20):
+        summoner_data = await self.get_summoner_by_puuid(puuid)
         if(summoner_data["status_code"] != 200):
             return [[], summoner_data]
         summoner_puuid = summoner_data["puuid"]
@@ -87,23 +97,23 @@ class RiotAPI:
         game_info = GameInfo(id, start_time, game_duration, winner, participants, queue_type)
         return game_info
         
-    async def get_recent_matches_infos(self, username, count=20):
+    async def get_recent_matches_infos(self, puuid, count=20):
         matches_infos = []
-        data = await self.get_recent_matches_ids(username, count)
+        data = await self.get_recent_matches_ids(puuid, count)
         for match_id in data[0]:
             match_info = await self.get_match_info_by_id(match_id)
             if(match_info is not None):
                 matches_infos.append(match_info)
         return [matches_infos, data[1]]
 
-    async def get_recent_match_info(self, username, id=0):
-        match_data = await self.get_recent_matches_ids(username, id+1)
+    async def get_recent_match_info(self, puuid, id=0):
+        match_data = await self.get_recent_matches_ids(puuid, id+1)
         if(len(match_data[0]) > id):
             return await self.get_match_info_by_id(match_data[0][id])
         return None
 
     async def get_ranked_info(self, user_id):
-        url = f"{self.base_url}league/v4/entries/by-summoner/{user_id}"
+        url = f"{self.base_url}lol/league/v4/entries/by-summoner/{user_id}"
         params = {'api_key': self.api_key}
         ranks = []
         async with aiohttp.ClientSession() as session:
@@ -123,7 +133,7 @@ class RiotAPI:
         return ranks
 
     async def get_mastery_info(self, user_id):
-        url = f"{self.base_url}champion-mastery/v4/champion-masteries/by-summoner/{user_id}"
+        url = f"{self.base_url}lol/champion-mastery/v4/champion-masteries/by-summoner/{user_id}"
         params = {'api_key': self.api_key}
         champions = []
         async with aiohttp.ClientSession() as session:
@@ -140,8 +150,8 @@ class RiotAPI:
 
 
 
-    async def get_profile_info(self, username):
-        summoner = await self.get_summoner_by_name(username)
+    async def get_profile_info(self, puuid):
+        summoner = await self.get_summoner_by_puuid(puuid)
         if(summoner["status_code"] != 200):
             return {"status_code": 404, "message": "Summoner not found"}
         id = summoner["id"]
